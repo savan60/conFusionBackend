bb9221bef03bdb73c30ed91e0e24fb3905b0af2f
savan60 <51092808+savan60@users.noreply.github.com>
2021-07-03 23:12:38 +0530

    express session part 2

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session=require('express-session');
+var FileStore=require('session-file-store')(session);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -27,38 +28,41 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser('1234567890-savan'));
+app.use(session({
+  name:'session-id',
+  secret:'savan-is-great',
+  saveUninitialized:false,
+  resave:false,
+  store:new FileStore()
+}));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 function auth(req,res,next){
-  console.log(req.headers);
-  var authHeader=req.headers.authorization;
-
-  if(!authHeader){
-    var err=new Error("You are not authenticated");
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
-    return next(err);
-  }
-  var auth=new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
-  var username=auth[0];
-  var password=auth[1];
-
-  if(username==="admin" && password==="password"){
-    next();
+  console.log(req.session);
+  if(!req.session.user){
+      var err=new Error("You are not authenticated");
+      err.status=401;
+      return next(err);
   }
   else{
-    var err=new Error("You are not authenticated");
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
-    return next(err);
+    if(req.session.user==="authenticated"){
+      next();
+    }
+    else{
+      var err=new Error("You are not authenticated");
+      err.status=403;
+      return next(err);
+    }
   }
 }
+
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/leaders', LeaderRouter);
 app.use('/promotions', PromoRouter);
